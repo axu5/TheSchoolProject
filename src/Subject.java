8418235097304aa80421b6adbc1a9c4e16cbe23e
crystal cabins <crystalcabinss@gmail.com>
2022-02-25 09:18:17 +0100

    adding comments

--- src/Subject.java
+++ src/Subject.java
@@ -83,17 +83,21 @@ public class Subject {
   }
 
   public void setTeacher(Teacher teacher) {
+    // add the teacher reference to this class, and add the subject reference into
+    // the teacher
     this.teacher = teacher;
     teacher.addCourse(this);
   }
 
   public void printStudentNames() {
+    // concatenate an empty string with commas for all the names.
     String cumulation = "";
     for (int i = 0; i < this.numStudents; ++i) {
-      Student current = this.students[i];
-      cumulation += (i > 0 ? ", " : "") + current.getName();
+      String studentName = this.students[i].getName();
+      cumulation += (i > 0 ? ", " : "") + studentName;
     }
 
+    // print the final result onto a new line
     System.out.println(cumulation);
   }
 
@@ -102,12 +106,15 @@ public class Subject {
    * @return return false if the course exists
    */
   public boolean addStudent(Student student) {
+    // loop over the students, return out if student is found, do not create
+    // duplicates
     for (int i = 0; i < numStudents; ++i) {
       Student current = this.students[i];
       if (current.getName().equals(student.getName()))
         return false;
     }
 
+    // set to the last position of the students array
     this.students[numStudents++] = student;
     return true;
   }
@@ -117,31 +124,48 @@ public class Subject {
    * @return return if the operation was successful
    */
   public boolean removeStudent(Student student) {
+    // make a variable for the offset of the items that will replace items in the
+    // array
     int removedOffset = 0;
     for (int i = 0; i < numStudents; ++i) {
+      // set current
       Student current = this.students[i];
+      // if this student name is being removed, add to offset counter
       if (current.getName().equals(student.getName())) {
         removedOffset++;
         student.removeCourse(this);
       }
 
+      // replace current index with an item from further on in the array.
       this.students[i] = this.students[i + removedOffset];
     }
+
+    // remove the amount of students removed.
     numStudents -= removedOffset;
 
+    // remove the course from the student as well.
+    // this works because you can remove courses that don't exist in the student,
+    // nothing will happen
     student.removeCourse(this);
 
+    // if removedOffset is 0 the student wasn't found in the class and this returns
+    // false
     return removedOffset > 0;
   }
 
   public double getAverageGrade() {
     double total = 0;
 
+    // O(n^2) time because of subjects loop inside get course grade.
+    // Teacher didn't mind, but it is something to keep in mind if I want to
+    // refactor.
     for (int i = 0; i < numStudents; ++i) {
+      // add everything to total
       Student current = this.students[i];
       total += current.getCourseGrade(this.courseName);
     }
 
+    // return average grade
     return total / numStudents;
   }
 }
